9eea40699698ec089304a9996fbbdbd7957217bf
Minerva Yan <xitingyan99@gmail.com>
2025-03-14 14:27:53 -0400

    Fixed redundant code files

--- alpacaFunctions.h
+++
@@ -1,7 +0,0 @@
-#include "contextStructs.h"
-
-#define MAX_DIR 10
-#define MAX_CONTXT_CHAR 5000
-
-void createDir(struct contextDir dirArr[MAX_DIR]);
-void createFile(struct contextDir dirArr[MAX_DIR]);
--- contextStructs.h
+++
@@ -1,8 +0,0 @@
-//This header file defines the struct that will represent directories and any relevant maximums.
-#define MAX_TITLE_CHAR 20
-#define MAX_FILES 50
-
-struct contextDir {
-    char dirTitle[MAX_TITLE_CHAR + 1]; // + 1 to include null terminator.
-    char fileTitle[MAX_FILES][MAX_TITLE_CHAR + 1]; // + 1 to include null terminator.
-};
